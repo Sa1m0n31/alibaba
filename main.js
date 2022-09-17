ffab7f4628afa41535784a9fad0c1b62c521c9c9
Szymon Burak <sajmon0031@gmail.com>
2022-09-17 21:19:38 +0200

    Add mobile version for homepage

---
+++ main.js
@@ -0,0 +1,20 @@
+const mobileMenu = document.querySelector('.mobileMenu');
+const mobileMenuChildren = Array.from(document.querySelectorAll('.mobileMenu>*'));
+
+const openMobileMenu = () => {
+    mobileMenu.style.transform = 'scaleX(1)';
+    setTimeout(() => {
+        mobileMenuChildren.forEach((item) => {
+            item.style.opacity = '1';
+        });
+    }, 200);
+}
+
+const closeMobileMenu = () => {
+    mobileMenuChildren.forEach((item) => {
+        item.style.opacity = '0';
+    });
+    setTimeout(() => {
+        mobileMenu.style.transform = 'scaleX(0)';
+    }, 200);
+}
