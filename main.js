0f70203ce55947f11a7c4065e9ee50549b412185
Szymon Burak <sajmon0031@gmail.com>
2022-10-05 02:51:33 +0200

    Style changes, switching texts and images

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ const closeMobileMenu = () => {
         mobileMenu.style.transform = 'scaleX(0)';
     }, 200);
 }
+
+let companyContentVisible = [false, false, false];
+const companyContentButtonsContent = Array.from(document.querySelectorAll('.aboutUs__item__btn>span'));
+const companyContentButtons = Array.from(document.querySelectorAll('.aboutUs__item__btn'));
+const companyContentArrows = Array.from(document.querySelectorAll('.aboutUs__item__btn>img'));
+const companyContentText = Array.from(document.querySelectorAll('.aboutUs__item__text'));
+
+const toggleCompanyContent = (n) => {
+    companyContentVisible = companyContentVisible.map((item, index) => {
+        if(index === n) return !item;
+        else return item;
+    });
+
+    console.log(companyContentText[n]);
+
+    companyContentButtonsContent[n].textContent = companyContentVisible[n] ? 'Zwiń' : 'Rozwiń';
+    companyContentButtons[n].style.marginBottom = companyContentVisible[n] ? '20px' : '0';
+    companyContentArrows[n].style.transform = companyContentVisible[n] ? 'rotateX(180deg)' : 'none';
+    companyContentText[n].style.visibility = companyContentVisible[n] ? 'visible' : 'hidden';
+    companyContentText[n].style.height = companyContentVisible[n] ? 'auto' : '0';
+}
